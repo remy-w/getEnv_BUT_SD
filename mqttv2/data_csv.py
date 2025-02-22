46253a4467519683449bfaf094f18cd35b151ee1
remy-w <remycourte1@icloud.com>
2025-02-22 18:19:33 +0100

    Update data_csv.py

--- mqttv2/data_csv.py
+++ mqttv2/data_csv.py
@@ -4,12 +4,12 @@ import threading
 from datetime import datetime
 import re
 
-# Configuration MQTT
+
 ADRESSE_IP = '10.42.0.242'
 PORT = 1883
 TOPIC = 'test_channel'
 
-# Listes pour chaque type de données
+
 timestamps = []
 humidite = []
 temperature = []
@@ -17,19 +17,19 @@ co2 = []
 nano_particules = []
 son = []
 
-# Fonction pour analyser le message et extraire les données
+
 def parse_message(message):
     pattern = r'humidité\s*:\s*([\d.]+)|temperature\s*:\s*([\d.]+)|co2\s*:\s*(\d+)|nano particules\s*:\s*(\d+)|son\s*:\s*(\d+)'
     matches = re.findall(pattern, message)
     
-    data = [None] * 5  # Pour stocker les valeurs dans l'ordre
+    data = [None] * 5 
     for match in matches:
         for i, value in enumerate(match):
             if value:
                 data[i] = float(value) if '.' in value else int(value)
     return data
 
-# Fonction appelée lorsqu'un message est reçu
+
 def on_message(client, userdata, msg):
     message = msg.payload.decode()
     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -37,7 +37,7 @@ def on_message(client, userdata, msg):
     
     timestamps.append(timestamp)
     
-    # Extraction des valeurs
+    
     values = parse_message(message)
     humidite.append(values[0])
     temperature.append(values[1])
@@ -45,12 +45,12 @@ def on_message(client, userdata, msg):
     nano_particules.append(values[3])
     son.append(values[4])
 
-# Écriture périodique des données dans un CSV
+
 def save_data_to_csv():
     if timestamps:
         with open('mqtt_data.csv', mode='a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
-            # Écrit les en-têtes si le fichier est vide
+            
             if file.tell() == 0:
                 writer.writerow(["Timestamp", "Humidité", "Température", "CO₂", "Nano Particules", "Son"])
             for i in range(len(timestamps)):
@@ -71,14 +71,14 @@ def save_data_to_csv():
         son.clear()
     threading.Timer(60, save_data_to_csv).start()
 
-# Initialisation MQTT
+
 client = mqtt.Client()
 client.on_connect = lambda c, u, f, r: c.subscribe(TOPIC)
 client.on_message = on_message
 client.connect(ADRESSE_IP, PORT, 60)
 
-# Démarre l'écriture périodique dans le fichier CSV
+
 save_data_to_csv()
 
-# Écoute des messages MQTT
+
 client.loop_forever()
